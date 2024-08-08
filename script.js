ffa9b923f6b229bc73b3807f8633ef854a927b75
Andre Vu <andre.vu.ms@gmail.com>
2024-08-07 23:17:50 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const data = [
+    { name: "Alice", price: 30, occupation: "writer" },
+    { name: "Bob", price: 50, occupation: "teacher" },
+    { name: "Carol", price: 70, occupation: "programmer" }
+];
